101492774fc098795936a8822050dd655c481280
Melissa Kerr <melissakerr1821@gmail.com>
2019-05-15 17:39:11 +1200

    Finished Arrays

--- Basics/script.js
+++ Basics/script.js
@@ -330,4 +330,32 @@ if (height === '23') {
  * ARRAYS
  *********/
 
- newArray = [];
+ // Initialize new array
+ var names = ['John', 'Mark', 'Jane'];
+ var years = new Array(1990, 1969, 1948);
+
+ console.log(names);
+ console.log(names[1]);
+ console.log(names.length);
+
+// Mutate Array Data
+ names[1] = 'Ben';
+ names[names.length] = 'Mary';
+ console.log(names);
+
+ // Different data types
+ var john = ['John', 'Smith', 1990, 'designer', false];
+
+ john.push('blue');
+ john.unshift('Mr.');
+ console.log(john);
+
+ john.pop();
+ john.pop();
+ john.shift();
+ console.log(john);
+
+ console.log(john.indexOf(1990));
+
+ var isDesigner = john.indexOf('designer') === -1 ? 'John is not a Designer' : 'John is a Designer';
+console.log(isDesigner);
