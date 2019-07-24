d15411f4f3107620796e5f0040253ced8337ad19
Melissa Kerr <melissakerr1821@gmail.com>
2019-07-24 20:34:31 +1200

    Started Section 3 - Hoisting, Scope and this Keyword

--- Section3/script.js
+++ Section3/script.js
@@ -1,19 +1,37 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// calculateAge(1965);
 
+// function calculateAge(year) {
+//     console.log(2016 - year);
+// }
 
+// retirement(1965); This won't work because
+// it is a function expression not a 
+// function decleration like above
 
+// var retirement = function(year) {
+//     console.log(65 - (2016 - year));
+// }
 
+// variables
+//console.log(age); This won't work because
+// in the creation phase of the object 
+// the code is scanned for variable
+// declarations and set to undefined.
+// var age = 23;
 
+// function foo() {
+//     var age = 65;
+//     console.log(age);
+// }
+// foo();
+// console.log(age);
 
-
-
-
-
-
-
-
+// Basically, hoisting can be used to 
+// declare functions before the have been
+// declared.
 
 
 
@@ -39,7 +57,6 @@ function first() {
 */
 
 
-
 // Example to show the differece between execution stack and scope chain
 
 /*
@@ -58,10 +75,9 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    console.log(a+d);
 }
 */
-
 
 
 ///////////////////////////////////////
