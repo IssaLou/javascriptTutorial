fc3fe71bb4da1130b3c120a2ebb053898f75b29f
Melissa Kerr <melissakerr1821@gmail.com>
2019-07-30 09:31:33 +1200

    Finished Section 3

--- Section3/script.js
+++ Section3/script.js
@@ -92,9 +92,45 @@ function third() {
 // The this keyword is not assigned a value until a 
 // function where it is actually defined is actually called. 
 
+// Examples:
+// console.log(this); --> Refers to the window as it is the default.
+/*
+calculateAge(1985);
 
+function calculateAge(year) {
+    console.log(2016 - year);
+    console.log(this);
+}
+*/
 
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
+/*
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+*/
+    }
+};
 
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
+
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
+
+// REMEMBER: We use paranthesis to call a function but we can
+// treat the function as a variable by not using the paranthesis
+// when we want to use it.
 
 
 
