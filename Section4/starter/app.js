1f7ac6c0e8c0f52c361bcb1e8ed260c5bb3e5260
Melissa Kerr <melissakerr1821@gmail.com>
2019-08-14 22:31:38 +1200

    Update the scores and changed the active player

--- Section4/starter/app.js
+++ Section4/starter/app.js
@@ -22,27 +22,53 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 //Events and Event Handling: Rolling the Dice
 
-document.querySelector('#btn-roll').addEventListener('click', function() {;
+document.querySelector('.btn-roll').addEventListener('click', function() {;
     // Do something in this annonymous function.
     //1. Random number
+    let dice = Math.floor(Math.random() * 6) +1;
 
     //2. Display the result
-
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png'
     //3. Update the round score if the rolled number was NOT a 1
+    if(dice !== 1) {
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+        //These commented out below removes a tag when something happens in the HTML file and can add a tag too.
+        //The other two do exactle the same thing but toggle between the two HTML tags which is dependent on the conditions above.
+        //document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
+
 });
 
 
