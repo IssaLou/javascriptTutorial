58d2ab4190ca72816d0e47d21fde88deb1775c10
Melissa Kerr <melissakerr1821@gmail.com>
2019-08-26 18:35:01 +1200

    Added user input to control the winning score and styled it

--- Section4/starter/challenges.js
+++ Section4/starter/challenges.js
@@ -53,9 +53,19 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
         //Update the UI 
         document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
-    
+
+        let input = document.querySelector('.final-score').value;
+        let winningScore;
+        // Type coercion: values that are undefined, 0, "" or null are COERCED to FALSE
+        // Anything else is COERCED to TRUE
+        if(input) {
+            winningScore = input;
+        } else {
+            winningScore = 100;
+        }
+
         //Check if player won the game
-        if(scores[activePlayer] >= 100) {
+        if(scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
