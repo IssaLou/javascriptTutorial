ef2e0354ef36ae651c03b30f766f3a4580849b5d
Melissa Kerr <melissakerr1821@gmail.com>
2019-11-20 14:39:30 +1300

    Function Constructors

---
+++ Section5/starter/script.js
@@ -0,0 +1,35 @@
+// Function Constructor
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+// Function Constructors is always with a capital letter
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+    console.log(2016 - this.yearOfBirth);
+}
+
+Person.prototype.lastName = 'Smith';
+
+//This is instansiation; as this is an instance 
+//of the Person Object
+//'new' = a new empty object
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
