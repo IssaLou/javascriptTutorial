9aafc46100db118ae1d482765e864f4247b6ed65
Melissa Kerr <Melissa.Kerr@airnz.co.nz>
2020-04-19 11:04:15 +1200

    Just finsihed IIFE

--- Section5/starter/script.js
+++ Section5/starter/script.js
@@ -1,5 +1,6 @@
 // Function Constructor
-
+// General way of creating an object. Not very useful if you have to create multiple objects
+/* 
 var john = {
     name: 'John',
     yearOfBirth: 1990,
@@ -33,3 +34,148 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+*/
+
+// Object.create
+/*
+var personProto = {
+  calculateAge: function() {
+    console.log(2019 - this.yearOfBirth);
+  }
+};
+
+var john = Object.create(personProto);
+john.name = "John";
+john.yearOfBirth = 1990;
+john.job = "teacher";
+
+var jane = Object.create(personProto, {
+  name: { value: "Jane" },
+  yearOfBirth: { value: 1969 },
+  job: { value: "designer" }
+});
+*/
+
+// Primitives vs objects
+//Primitives
+/*
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+//Objects
+var obj1 = {
+  name: "John",
+  age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+  name: "Mel",
+  city: "Auckland"
+};
+
+function change(a, b) {
+  a = 30;
+  b.city = "San Francisco";
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+*/
+
+// Passing functions as arguments
+/*
+var years = [1990, 1965, 1937, 2005, 1988];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2016 - el;
+}
+
+function isFullAge(el) {
+  return el >= 18;
+}
+
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - 0.67 * el);
+  } else {
+    return -1;
+  }
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(rates);
+*/
+
+// Functions returning another function
+/*
+function interviewQuestion(job) {
+  if (job === "designer") {
+    return function(name) {
+      console.log(name + ", can you please explain what UX design is?");
+    };
+  } else if (job === "teacher") {
+    return function(name) {
+      console.log("What subject do you teach, " + name + "?");
+    };
+  } else {
+    return function(name) {
+      console.log("Hello " + name + ", what do you do?");
+    };
+  }
+}
+
+var teacherQuestion = interviewQuestion("teacher");
+var designerQuestion = interviewQuestion("designer");
+
+teacherQuestion("John");
+designerQuestion("John");
+designerQuestion("Jane");
+designerQuestion("Mark");
+designerQuestion("Mike");
+
+interviewQuestion("teacher")("Mark");
+*/
+
+// Immediately Invoked Function Expressions(IIFE)
+
+// function game() {
+//   var score = Math.random() * 18;
+//   console.log(score >= 5);
+// }
+
+// game();
+
+(function() {
+  var score = Math.random() * 10;
+  console.log(score >= 5);
+})();
+
+(function(goodLuck) {
+  var score = Math.random() * 10;
+  console.log(score >= 5 - goodLuck);
+})(5);
