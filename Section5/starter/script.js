54289e86a7dbe6cf139f80d046a082ed13fcf027
Melissa Kerr <Melissa.Kerr@airnz.co.nz>
2020-04-20 14:31:23 +1200

    Nearly finished, just need to fix up some minor bugs in the final solution to the challenge

--- Section5/starter/script.js
+++ Section5/starter/script.js
@@ -1,13 +1,13 @@
 // Function Constructor
 // General way of creating an object. Not very useful if you have to create multiple objects
 /* 
-var john = {
+let john = {
     name: 'John',
     yearOfBirth: 1990,
     job: 'teacher'
 };
 // Function Constructors is always with a capital letter
-var Person = function(name, yearOfBirth, job) {
+let Person = function(name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
@@ -23,9 +23,9 @@ Person.prototype.lastName = 'Smith';
 //of the Person Object
 //'new' = a new empty object
 
-var john = new Person('John', 1990, 'teacher');
-var jane = new Person('Jane', 1969, 'designer');
-var mark = new Person('Mark', 1948, 'retired');
+let john = new Person('John', 1990, 'teacher');
+let jane = new Person('Jane', 1969, 'designer');
+let mark = new Person('Mark', 1948, 'retired');
 
 john.calculateAge();
 jane.calculateAge();
@@ -38,18 +38,18 @@ console.log(mark.lastName);
 
 // Object.create
 /*
-var personProto = {
+let personProto = {
   calculateAge: function() {
     console.log(2019 - this.yearOfBirth);
   }
 };
 
-var john = Object.create(personProto);
+let john = Object.create(personProto);
 john.name = "John";
 john.yearOfBirth = 1990;
 john.job = "teacher";
 
-var jane = Object.create(personProto, {
+let jane = Object.create(personProto, {
   name: { value: "Jane" },
   yearOfBirth: { value: 1969 },
   job: { value: "designer" }
@@ -59,25 +59,25 @@ var jane = Object.create(personProto, {
 // Primitives vs objects
 //Primitives
 /*
-var a = 23;
-var b = a;
+let a = 23;
+let b = a;
 a = 46;
 console.log(a);
 console.log(b);
 
 //Objects
-var obj1 = {
+let obj1 = {
   name: "John",
   age: 26
 };
-var obj2 = obj1;
+let obj2 = obj1;
 obj1.age = 30;
 console.log(obj1.age);
 console.log(obj2.age);
 
 // Functions
-var age = 27;
-var obj = {
+let age = 27;
+let obj = {
   name: "Mel",
   city: "Auckland"
 };
@@ -95,12 +95,12 @@ console.log(obj.city);
 
 // Passing functions as arguments
 /*
-var years = [1990, 1965, 1937, 2005, 1988];
+let years = [1990, 1965, 1937, 2005, 1988];
 
 function arrayCalc(arr, fn) {
-  var arrRes = [];
+  let arrRes = [];
 
-  for (var i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     arrRes.push(fn(arr[i]));
   }
 
@@ -123,9 +123,9 @@ function maxHeartRate(el) {
   }
 }
 
-var ages = arrayCalc(years, calculateAge);
-var fullAges = arrayCalc(ages, isFullAge);
-var rates = arrayCalc(ages, maxHeartRate);
+let ages = arrayCalc(years, calculateAge);
+let fullAges = arrayCalc(ages, isFullAge);
+let rates = arrayCalc(ages, maxHeartRate);
 
 console.log(ages);
 console.log(rates);
@@ -149,8 +149,8 @@ function interviewQuestion(job) {
   }
 }
 
-var teacherQuestion = interviewQuestion("teacher");
-var designerQuestion = interviewQuestion("designer");
+let teacherQuestion = interviewQuestion("teacher");
+let designerQuestion = interviewQuestion("designer");
 
 teacherQuestion("John");
 designerQuestion("John");
@@ -164,18 +164,266 @@ interviewQuestion("teacher")("Mark");
 // Immediately Invoked Function Expressions(IIFE)
 
 // function game() {
-//   var score = Math.random() * 18;
+//   let score = Math.random() * 18;
 //   console.log(score >= 5);
 // }
 
 // game();
 
+/*
 (function() {
-  var score = Math.random() * 10;
+  let score = Math.random() * 10;
   console.log(score >= 5);
 })();
 
 (function(goodLuck) {
-  var score = Math.random() * 10;
+  let score = Math.random() * 10;
   console.log(score >= 5 - goodLuck);
 })(5);
+*/
+
+// Closures
+/*
+function retirement(retirementAge) {
+  let a = " years left until retirement.";
+  return function(yearOfBirth) {
+    let age = 2016 - yearOfBirth;
+    console.log(retirementAge - age + a);
+  };
+}
+
+let retirementUS = retirement(66);
+let retirementGermany = retirement(65);
+let retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+*/
+// retirement(66)(1990);
+
+// Closures are inner functions that always have access to the letiables and parameters of its outer function, even after the outer function has returned.
+/*
+function interviewQuestion(job) {
+  return function(name) {
+    if (job === "designer") {
+      console.log(name + ", can you please explain what UX design is?");
+    } else if (job === "teacher") {
+      console.log("What subject do you teach, " + name + "?");
+    } else {
+      console.log("Hello " + name + ", what do you do?");
+    }
+  };
+}
+
+let teacherQuestion = interviewQuestion("teacher");
+let designerQuestion = interviewQuestion("designer");
+
+teacherQuestion("John");
+designerQuestion("John");
+designerQuestion("Jane");
+designerQuestion("Mark");
+designerQuestion("Mike");
+
+interviewQuestion("teacher")("Mark");
+*/
+
+// Bind, call and apply
+/*
+let john = {
+  name: "John",
+  age: 26,
+  job: "teacher",
+  presentation: function(style, timeOfDay) {
+    if (style === "formal") {
+      console.log(
+        "Good " +
+          timeOfDay +
+          " lades and gentlemen! I'm " +
+          this.name +
+          ", I'm a " +
+          this.job +
+          " and I'm " +
+          this.age +
+          " years old."
+      );
+    } else if (style === "friendly") {
+      console.log(
+        "Hay! What's up? I'm " +
+          this.name +
+          ", I'm a " +
+          this.job +
+          " and I'm " +
+          this.age +
+          " years old. Have a nice " +
+          timeOfDay +
+          "."
+      );
+    }
+  }
+};
+
+let emily = {
+  name: "Emily",
+  age: 35,
+  job: "designer"
+};
+
+john.presentation("formal", "morning");
+
+john.presentation.call(emily, "friendly", "afternoon");
+
+// john.presentation.apply(emily, ["friendly", "afternoon"]);
+
+// A form of currying.
+let johnFriendly = john.presentation.bind(john, "friendly");
+
+johnFriendly("evening");
+
+let emilyFormal = john.presentation.bind(emily, "formal");
+emilyFormal("afternoon");
+
+// An example of using binding to preset a function argument.
+let years = [1990, 1965, 1937, 2005, 1988];
+
+function arrayCalc(arr, fn) {
+  let arrRes = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2016 - el;
+}
+
+function isFullAge(limit, el) {
+  return el >= limit;
+}
+
+let ages = arrayCalc(years, calculateAge);
+let fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+
+console.log(ages);
+console.log(fullJapan);
+*/
+//////////////////////////
+/// CODING CHALLENGE 7 ///
+//////////////////////////
+
+/*
+Building a quiz game.
+
+1. Build a function constructor called Question to describe a questions. A question should include:
+  a) The question itself
+  b) The answer from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+  c) Correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor.
+
+3. Store them all in an array.
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (HINT: Write a method for the Questions objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct or not (HINT: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code.
+
+EXPERT LEVEL:
+
+8. After you display the result, display the next random question, so that the game never ends (HINT: write a function for this and call it right after displaying the result).
+
+9. Be careful: after Task 8, the game literally never ends. So include an option to quit the game if the user writes 'exit' instead of the answer. In this case DON'T call the function from Task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (HONT: I'm going to use the power of closures for this, but you don't have to).
+
+11. Display the score in the console. Use yet another method for this.
+*/
+
+(function() {
+  let Question = function(query, possibleAns, correctAns) {
+    this.query = query;
+    this.possibleAns = possibleAns;
+    this.correctAns = correctAns;
+  };
+
+  Question.prototype.displayQuestion = function() {
+    console.log(this.query);
+
+    for (let i = 0; i < this.possibleAns.length; i++) {
+      console.log(i + ": " + this.possibleAns[i]);
+    }
+  };
+
+  Question.prototype.checkAnswer = function(ans, callback) {
+    let sc;
+
+    if (ans === this.correctAns) {
+      console.log("Correct answer!");
+      sc = callback(true);
+    } else {
+      console.log("Incorrect! Try again");
+      sc = callback(false);
+    }
+
+    this.displayScore(sc);
+  };
+
+  Question.prototype.displayScore = function(score) {
+    console.log("Your current score is: " + score);
+    console.log("------------------------------");
+  };
+
+  let qOne = new Question(
+    "Is the JavaScript language the best language ever?",
+    ["Yes", "No", "Maybe?"],
+    0
+  );
+  let qTwo = new Question(
+    "What is the name of the teacher in this couse?",
+    ["John", "Jane", "Jonas", "Mark"],
+    2
+  );
+  let qThree = new Question(
+    "Are we still in lockdown?",
+    ["No", "Yes", "What's lockdown?"],
+    1
+  );
+  let questionArr = [qOne, qTwo, qThree];
+
+  function score() {
+    let sc = 0;
+    return function(correct) {
+      if (correct) {
+        sc++;
+      }
+      return sc;
+    };
+  }
+  let keepScore = score();
+
+  function keepPlaying() {
+    let n = Math.floor(Math.random() * questionArr.length);
+    questionArr[n].displayQuestion();
+
+    let answer = parseInt(prompt("Please select the correct answer."));
+    questionArr[n].checkAnswer(answer);
+
+    if (
+      answer.toString() !== "quit" ||
+      answer.toString() !== "Quit" ||
+      answer.toString() !== "exit" ||
+      answer.toString() !== "Exit"
+    ) {
+      questionArr[n].checkAnswer(answer, keepScore);
+    }
+
+    keepPlaying();
+  }
+  keepPlaying();
+})();
